0e5ed12157adc5314b2659d39e2991aab0c4455e
jasonboller <88179982+jasonboller@users.noreply.github.com>
2022-11-02 18:12:06 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,18 +1,19 @@
-// 霸都丶傲天 2019年10月10日 https://github.com/AJLoveChina/birthday
+// 哈哈 https://github.com/AJLoveChina/birthday
 var config = {
     // 句子的长度可以任意， 你可以写十句话， 二十句话都可以
     // 每句话尽量不要超过15个字,不然展示效果可能不太好
     texts: [
-        "送给我",      //这里,每句话结尾的最后一个逗号必须是英文的哦!! 很重要哦!!
-        "心爱的小可爱",  // 同上...
+        "送给我为数不多的好朋友",      //这里,每句话结尾的最后一个逗号必须是英文的哦!! 很重要哦!!
+        "初识于冬天",  // 同上...
+        "虽然只见过几次面",
+        "但是异乡中的老乡",
+        "我可不想就此作罢~哈哈哈",
         "今天是你的生日",
-        "这是我们在一起的",
-        "第三个生日了哦",
-        "去年的生日",
-        "还记得在王婆串串吃的呢",
-        "今年要吃好的喽哦",
-        "要把我家可爱猪猪喂饱饱",
-        "然后抱走",
+        "生日快乐噢！",
+        "寿星buff加成",
+        "霉运跑光光，好运连连来",
+        "吃好玩好~好好睡个觉觉",
+        "时而隐身时而碎嘴的老张",
         "YAMI~~",
     ],
     /**
@@ -25,17 +26,17 @@ var config = {
      * Tip: 图片最好用正方形or接近正方形, 看起来效果更好
      */
     imgs: {
-        "心爱的小可爱": "./imgs/xiaokeai.png",
+        "生日快乐噢！": "./imgs/xiaokeai.png",
         // "今天是你的生日": "./imgs/birthday.jpg",
     },
     // 按钮文字描述, 以下是默认的按钮文字，英文的，您可以改成你喜欢的文字
     desc: {
-        turn_on: "开始",
-        play: "音乐",
-        bannar_coming: "颜色",
+        turn_on: "~~戳戳我~~",
+        play: "MUSIC>>",
+        bannar_coming: "surprise>>>",
         balloons_flying: "好像少点东西",
-        cake_fadein: "蛋糕？",
-        light_candle: "蜡烛？",
+        cake_fadein: "蛋糕？？",
+        light_candle: "蜡烛？？",
         wish_message: "生日快乐",
         story: "A MESSAGE FOR YOU",
     }
